373e4e0bd0a6e2306e700df6a5d792c2bec49b87
Vitaliy Bogatov <x-vitaliy.bogatov@coyote.com>
2021-03-10 00:22:51 +0200

    Basic implementation

---
+++ TI_Practice4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Practice4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write something.");
+            String input = Console.ReadLine();
+
+            if (input.Length > 1)
+            {
+                String output = "";
+                char[] array = input.ToCharArray();
+
+                foreach (char element in array)
+                {
+                    output = element + output;
+                }
+
+                if (input == output)
+                {
+                    Console.WriteLine("You have written a palindrome.");
+                }
+                else
+                {
+                    Console.WriteLine("Written string is not a palindrome.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have written a palindrome.");
+            }
+        }
+    }
+}
